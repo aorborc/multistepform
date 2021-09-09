ee4190f362980f474468be8810cb67d262d37cd5
Ruben <aorborc@github.com>
2021-09-10 04:26:49 +0530

    test

--- js/form.js
+++ js/form.js
@@ -1,15 +1,14 @@
-ZOHO.CREATOR.init().then(function(data) {
-  console.log('data : ', data);
+ZOHO.CREATOR.init().then(function() {
   var queryParams = ZOHO.CREATOR.UTIL.getQueryParams();
   console.log('Query : ', queryParams);
-  formData = {};
-  var config = {
-    formName: 'timesheet',
-    data: formData
-  };
-  ZOHO.CREATOR.API.addRecord(config).then(function(response) {
-    if (response.code == 3000) {
-      console.log('Record added successfully');
-    }
-  });
+  // formData = {};
+  // var config = {
+  //   formName: 'timesheet',
+  //   data: formData
+  // };
+  // ZOHO.CREATOR.API.addRecord(config).then(function(response) {
+  //   if (response.code == 3000) {
+  //     console.log('Record added successfully');
+  //   }
+  // });
 });
